a8acf220d5a5fc49fe91fbd6c215c7e860febccf
EtherDaler <daler.hojimatov@yandex.ru>
2022-02-19 02:56:55 +0300

    run.sh debugged

--- task1/run.sh
+++ task1/run.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-dirpath = $1
-extension = $2
-foldname = $3
-archname = $4
-mkdir $foldname
-cp -r $dirpath $foldname
-for i in $(find $foldname -type f -not -name "*.$extension")
+
+CURR_LOCATION=$(pwd)
+
+mkdir $(pwd)/$3
+DESTINATION=${CURR_LOCATION}/$3
+
+FILE_LIST=$(find $1 -name "*.$2")
+
+counter=""
+
+for file in ${FILE_LIST}
 do
-	rm $i
+  TARGET_NAME=$(basename "$file")
+
+  while [[ -e ${DESTINATION}/"$TARGET_NAME" ]]
+  do
+    let counter++
+    TARGET_NAME=$counter"$TARGET_NAME"
+  done
+
+  cp "$file" ${DESTINATION}/"$TARGET_NAME"
 done
-for j in $(find $foldname -type d -empty)
-do
-	rm -d $j
-done
-tar -czpf $archname $foldname
+
+cd ${CURR_LOCATION}
+tar -zcf $4 $3
+
 echo done
-
-
-
