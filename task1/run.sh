b7cb6420620cb9cac90c777f8a30cd1eaec9373d
EtherDaler <daler.hojimatov@yandex.ru>
2022-03-03 20:09:53 +0300

    changed iteration

--- task1/run.sh
+++ task1/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for opt in 1 2 3 4
+while [ -n $1 ]
 do
     case $1 in
 	--input_folder) folder=$2;;
@@ -8,7 +8,7 @@ do
         --backup_folder) backup_f=$2;;
         --backup_archive_name) arch_name=$2;;
     esac
-    shift 2
+    shift
 done
 
 
