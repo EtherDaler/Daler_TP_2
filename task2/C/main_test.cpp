d02a8e8526c052e572f40d082a450406fc53edc3
EtherDaler <daler.hojimatov@yandex.ru>
2022-03-13 17:33:49 +0300

    task2 finally fixed especial CMake

--- task2/C/main_test.cpp
+++ task2/C/main_test.cpp
@@ -1,4 +1,16 @@
-#include "test.cpp"
+#include "main_test.h"
+
+TEST(CRectTest, delta)
+{
+        EXPECT_EQ(1, delta(2,1));
+        EXPECT_EQ(6, delta(5,-1));
+
+}
+
+TEST(CRectTest, multiply){
+        EXPECT_EQ(2, mult(2,1));
+        EXPECT_EQ(-5, mult(5,-1));
+}
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
---
+++ task2/C/main_test.h
@@ -0,0 +1,10 @@
+#include "A/index.h"
+#include "B/lib.h"
+#include <iostream>
+#include "gtest/gtest.h"
+
+class CRectTest : public ::testing::Test {
+};
+
+
+
