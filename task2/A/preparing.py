3577db3eccc71233fb16cf103e83618485472581
EtherDaler <63519670+EtherDaler@users.noreply.github.com>
2022-03-23 21:55:36 +0300

    fixed python script

--- task2/A/preparing.py
+++ task2/A/preparing.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-my_file = open("index.h", "w+")
+import sys
+
+my_file = open(sys.argv[1], "w+")
 my_file.write("#ifndef INDEX_H\n")
 my_file.write("#define INDEX_H\n")
 my_file.write("inline int delta(int x, int y){return x-y;}\n")
